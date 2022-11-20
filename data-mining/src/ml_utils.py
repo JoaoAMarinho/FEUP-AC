8b35864c872cf034ba1e109cfe7b7d302888ecd7
Beatriz Aguiar <beatrizaguiar.up@gmail.com>
2022-11-20 12:29:55 +0000

    create new df

--- data-mining/src/ml_utils.py
+++ data-mining/src/ml_utils.py
@@ -10,7 +10,7 @@ from imblearn.pipeline import Pipeline
 from imblearn.over_sampling import SMOTE
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import classification_report, auc, roc_curve, RocCurveDisplay, accuracy_score, f1_score, make_scorer, ConfusionMatrixDisplay
 
 
@@ -21,7 +21,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-from time import sleep
 
 def apply(
     df,
@@ -45,7 +44,7 @@ def apply(
     pipeline = []
 
     if feature_selection:
-        rfe = SequentialFeatureSelector(model_instance(), n_features_to_select=3)
+        rfe = SequentialFeatureSelector(model_instance(), n_features_to_select="auto", tol=0.07)
 
         if filter:
             rfe = SelectKBest(f_classif, k=10)
@@ -88,7 +87,7 @@ def apply(
     pipeline = []
     model_instance = model_instance(**parameter_grid)
     if feature_selection:
-        rfe = SequentialFeatureSelector(model_instance, n_features_to_select=3)
+        rfe = SequentialFeatureSelector(model_instance, n_features_to_select="auto", tol=0.07)
 
         if filter:
             rfe = SelectKBest(f_classif, k=10)
@@ -121,10 +120,11 @@ def apply(
         display(Markdown("## Neither oversample nor feature selection"))
 
 
+    score = pipe.score(X_test, y_test)
 
     display(Markdown(f"### **Classification report:** \n\n {classification_report(y_test, predictions)}"))
-    display(Markdown(f"### **Score:** \n\n {pipe.score(X_test, y_test)}"))
-    display(Markdown(f"### **Best parameters:** \n\n {grid_search.best_params_}"))
+    display(Markdown(f"### **Score:** \n\n {score}"))
+    display(Markdown(f"### **Best parameters:** \n\n {parameter_grid}"))
     display(Markdown("### **Confusion matrix:** "))
     ConfusionMatrixDisplay.from_estimator(pipe, X_test, y_test)
     plt.show()
@@ -140,4 +140,110 @@ def apply(
     display(Markdown("---"))
 
 
-    return pipe
+    return score
+
+
+def apply_cv(
+    df,
+    model_instance,
+    parameter_grid,
+    cross_validation=StratifiedKFold(n_splits=5),
+    feature_selection=False,
+    filter=False,
+    oversample=False
+):
+    parameter_tunning_sample = df.groupby('status', group_keys=False).apply(lambda x: x.sample(frac=0.3))
+    training_sample = df.drop(parameter_tunning_sample.index)
+
+    instance_parameter_grid = {}
+
+    for parameter_name, parameter_values in parameter_grid.items():
+        instance_parameter_grid[f"model__{parameter_name}"] = parameter_values
+
+    parameter_grid = instance_parameter_grid
+
+    pipeline = []
+
+    if feature_selection:
+        rfe = SequentialFeatureSelector(model_instance(), n_features_to_select="auto", tol=0.07)
+
+        if filter:
+            rfe = SelectKBest(f_classif, k=10)
+
+
+        pipeline.append(('feature_selection', rfe))
+
+    if oversample:
+        pipeline.append(('sampling', SMOTE(n_jobs=-1)))
+
+    pipeline.append(("model", model_instance()))
+
+    pipe = Pipeline(steps=pipeline)
+
+    # https://scikit-learn.org/stable/auto_examples/model_selection/plot_multi_metric_evaluation.html
+    scoring = {"AUC": "roc_auc", "Accuracy": make_scorer(
+        accuracy_score), "F1": make_scorer(f1_score)}
+
+    grid_search = GridSearchCV(
+        estimator=pipe,
+        param_grid=parameter_grid,
+        cv=cross_validation,
+        scoring=scoring,
+        refit="AUC"
+    )
+    
+    X = parameter_tunning_sample.drop(['status'], axis=1)
+    y = parameter_tunning_sample['status']
+
+    scaler = StandardScaler().fit(X)
+    X = pd.DataFrame(scaler.transform(X), index=X.index, columns=X.columns)
+    grid_search.fit(X, y)
+
+    # New pipeline
+    instance_parameter_grid = {}
+    for parameter_name, parameter_values in grid_search.best_params_.items():
+        instance_parameter_grid[parameter_name[7:]] = parameter_values
+    parameter_grid = instance_parameter_grid
+
+    pipeline = []
+    model_instance = model_instance(**parameter_grid)
+    if feature_selection:
+        rfe = SequentialFeatureSelector(model_instance, n_features_to_select="auto", tol=0.07)
+
+        if filter:
+            rfe = SelectKBest(f_classif, k=10)
+
+        pipeline.append(('feature_selection', rfe))
+
+    if oversample:
+        pipeline.append(('sampling', SMOTE(n_jobs=-1)))
+
+    pipeline.append(("model", model_instance))
+
+    pipe = Pipeline(steps=pipeline)
+
+    X = training_sample.drop(['status'], axis=1)
+    y = training_sample['status']
+    scaler = StandardScaler().fit(X)
+    X = pd.DataFrame(scaler.transform(X), index=X.index, columns=X.columns)
+
+    metrics = ['accuracy', 'roc_auc', 'precision']
+    scores = cross_validate(pipe, X, y, cv=5, scoring=metrics)
+
+    if (feature_selection & oversample):
+        display(Markdown("## Feature selection and oversample"))
+    elif (feature_selection):
+        display(Markdown("## Feature selection"))
+    elif (oversample):
+        display(Markdown("## Oversample"))
+    else:
+        display(Markdown("## Neither oversample nor feature selection"))
+
+
+    display(Markdown(f"### **Best parameters:** \n\n {parameter_grid}"))
+    for key in scores.keys():
+        display(Markdown(f"### **{key}**: {scores[key].mean()}"))
+                  
+    display(Markdown("---"))
+
+    return scores['test_accuracy'].mean()
